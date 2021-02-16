d2e45afe0bb015b8429d2982a37189cfae3d2831
trevorsaudi <isatrevor052@gmail.com>
2021-02-16 11:57:29 +0300

    partial complete

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_entry, only: [:show, :edit, :update, :destroy] #this line helps us render for show,edit,update
 
   # GET /entries
   # GET /entries.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'archives/index'
   resources :entries
   root to: "entries#index" #calls to the entries controller 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
